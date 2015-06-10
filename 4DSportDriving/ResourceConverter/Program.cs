45e6ff88ab8eb8cc5a97aa1eda5634bedf9fa9f1
warny_cp <warny@users.noreply.github.com>
2015-06-10 07:34:53 +0000

    corrections suite à la création de l'espace de nom pour l'agèbre linéaire

--- 4DSportDriving/ResourceConverter/Program.cs
+++ 4DSportDriving/ResourceConverter/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Mathematics;
+using Mathematics.LinearAlgebra;
 
 namespace ResourceConverter
 {
